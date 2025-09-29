be0b65179ca5ca82244f68035cc2f4807e8ffa25
Dragon_X <barron.samuel111@gmail.com>
2025-09-29 06:59:17 -0700

    Add React dependencies and plugin for proper build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import react from '@vitejs/plugin-react';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
       input: {
