55f3515b5f38b3710c02b9a699a2e27df1ab93c6
Dragon_X <barron.samuel111@gmail.com>
2025-09-28 19:01:37 -0700

    s

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import { useOptions } from '../utils/optionsContext';
+const Footer = () => {
+  return (
+    <div className="w-full fixed bottom-0 flex items-end justify-between p-2">
+    </div>
+  );
+};
+
+export default Footer;
